49b2bd55bc5b1d736f1b232a46d13ee2a83b1b19
vad2der <vad.deryabin@gmail.com>
2015-12-22 15:14:37 -0700

    Unified output functions. Added function to form simple list of objects with matching value for 'manufacturer' key

--- challenge.py
+++ challenge.py
@@ -22,7 +22,7 @@ def openProducts(pathfile):
 		l.append(json.loads(f))
 	return l
 
-def printProducts(products, start = None, finish = None):	
+def printObjects(products, start = None, finish = None):	
 	"""
 	represents list of products in defined scopes
 	"""
@@ -30,9 +30,15 @@ def printProducts(products, start = None, finish = None):
 		start = 0	
 	if finish is None:
 		finish = len(listing)
+	print ('\n')
+	print ('{} objects in list'.format(len(products)))
+	print ('See output for objects from {} to {} (not including):'.format(start, finish))
+	print ('\n')
 	for ind in range(start, finish):
 		print (ind, "--------------")
 		for k, v in products[ind].items():
+			k = (k.encode('cp1251', errors = 'ignore')).decode('utf-8', errors = 'ignore')
+			v = (v.encode('cp1251', errors = 'ignore')).decode('utf-8', errors = 'ignore')
 			print ('{} {} {}'.format(k, ": ", v))
 
 def openListing(pathfile):
@@ -46,22 +52,34 @@ def openListing(pathfile):
 		l.append(json.loads(f))
 	return l
 
-def printListing(listing, start = None, finish = None):
+def findMatches(template, source):
 	"""
-	represents list from listing in defined scopes
-	TODO: deal with encoding	
+	function takes
+		template (JSON object)
+		source (list of JSON objects)
+	looks for matches of template in source
+	returns a list
 	"""
-	if start is None:
-		start = 0	
-	if finish is None:
-		finish = len(listing)
-	for ind in range(start, finish):		
-		print (ind, "++++++++")
-		for k, v in listing[ind].items():
-			v = (v.encode('cp1251', errors = 'ignore')).decode('utf-8', errors = 'ignore')
-			print ('{} {} {}'.format(k, ": ", v))
+	result = []
+	foreign_key = None
+	for k1 in template.keys():
+		for k2 in source[0].keys():
+			if k1 == k2:
+				foreign_key = k1
+	if foreign_key is None:
+		print ('key reference has to be manually handeled')
+	foreign_key1 = foreign_key
+	foreign_key2 = foreign_key
+	for s in source:
+		if template[foreign_key1] in s[foreign_key2]:
+			result.append(s)	
+	return result
+
 	
+
 products = openProducts(path+products_file)
-#printProducts(products, 0, 3)
+#printObjects(products, 0, 1)
 listing = openListing(path+listing_file)
-printListing(listing, 4210, 4220)
+#printObjects(listing, 4210, 4211)
+matches = findMatches(products[0], listing)
+printObjects(matches, 0, 5)
