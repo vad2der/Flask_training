e63a3cebdb41c6f90b7167ab5ba1a5e8b27222c2
vad2der <vad.deryabin@gmail.com>
2015-12-23 10:28:11 -0700

    Eliminated range error in print  function. Started to work on 2nd step criteria in findMatches function

--- challenge.py
+++ challenge.py
@@ -26,10 +26,15 @@ def printObjects(products, start = None, finish = None):
 	"""
 	represents list of products in defined scopes
 	"""
+	# if start point is not set
 	if start is None:
-		start = 0	
+		start = 0
+	# if finish point is not set
 	if finish is None:
-		finish = len(listing)
+		finish = len(products)
+	# if finish point further than the range of the passed list
+	if ((finish != 0) and (len(products) < finish)):
+		finish = len(products)
 	print ('\n')
 	print ('{} objects in list'.format(len(products)))
 	print ('See output for objects from {} to {} (not including):'.format(start, finish))
@@ -74,18 +79,28 @@ def findMatches(template, source, foreign_key1 = None, foreign_key2 = None):
 		source (list of JSON objects)
 	looks for matches of template in source
 	returns a list
-	"""
-	result = []
+	"""	
+	# getting step 1 criteria
 	if ((foreign_key1 is None) and (foreign_key2 is None)):
 		foreign_key1, foreign_key2 = findForKeys(template, source[0])
-	# simple iterative search
+	# if smthng goes wrong
 	if ((foreign_key1 is None) and (foreign_key2 is not None)) or ((foreign_key1 is not None) and (foreign_key2 is None)):
 		print ('\nBoth foreign keys should be assigned.')
-	for s in source:
-		# match by manufacturer and model
-		if ((template[foreign_key1].lower() in s[foreign_key2].lower()) and (template['model'].lower() in s['title'].lower())):
-			result.append(s)	
-	return result
+	# match by foreign key - step 1
+	result1 = []
+	for s in source:		
+		if (template[foreign_key1].lower() in s[foreign_key2].lower()):
+			result1.append(s)
+	# match by having other records in - step 2
+	result2 = []	
+	for r in result1:
+		for k2, v2 in template.items():
+			if ((k2.lower() == foreign_key1) or ('price' in k2.lower()) or ('currency' in k2.lower())):
+				continue
+			for k1, v1 in r.items():
+				if (v2 in v1):
+					result2.append(r)		
+	return result2
 	
 
 products = openObjects(path+products_file)
